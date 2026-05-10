247bc358cb26d1fe8bffe7fdeb44abacb0b847b0
yomgolden <yomadeogun@gmail.com>
2026-05-10 20:57:57 +0100

    Initialize Express app with route handlers

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const matchRoutes = require("./routes/matchRoutes");
+const leaderboardRoutes = require("./routes/leaderboardRoutes");
+const shopRoutes = require("./routes/shopRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/user", userRoutes);
+app.use("/match", matchRoutes);
+app.use("/leaderboard", leaderboardRoutes);
+app.use("/shop", shopRoutes);
+
+module.exports = app;
