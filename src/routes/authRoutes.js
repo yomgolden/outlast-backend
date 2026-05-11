cbcd9ad1324573d8fcf8e94722571272f3918b1d
yomgolden <yomadeogun@gmail.com>
2026-05-11 12:53:54 +0100

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,15 +1,51 @@
 const express = require("express");
+
 const router = express.Router();
 
-const authenticateTelegram = require("../services/telegramAuth");
+const User = require("../models/User");
 
 router.post("/telegram", async (req, res) => {
+
   try {
-    const user = await authenticateTelegram(req.body);
+
+    const {
+      telegramId,
+      username
+    } = req.body;
+
+    let user =
+      await User.findOne({
+        telegramId
+      });
+
+    if (!user) {
+
+      user =
+        await User.create({
+
+          telegramId,
+
+          username,
+
+          gold: 1000,
+
+          gems: 0,
+
+          xp: 0,
+
+          level: 1,
+
+          playstyle: "BALANCED",
+
+          equippedTools: []
+
+        });
+    }
 
     res.json(user);
 
   } catch (error) {
+
     res.status(500).json({
       error: error.message
     });
