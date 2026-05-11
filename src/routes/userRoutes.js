67918e4dd11e967d33be578b67e07a60fef0dfcb
yomgolden <yomadeogun@gmail.com>
2026-05-11 16:26:29 +0100

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,59 +1,33 @@
-const express = require("express");
-const router = express.Router();
-
-const User = require("../models/User");
-const Tool = require("../models/Tool");
-
 router.get("/:userId", async (req, res) => {
+
   try {
-    const user = await User.findById(req.params.userId);
+
+    let user = await User.findById(
+      req.params.userId
+    );
+
+    if (!user) {
+
+      user = {
+        _id: req.params.userId,
+        username:
+          "Player_" +
+          req.params.userId.slice(-4),
+        gold: 1000,
+        gems: 50,
+        level: 1,
+        xp: 0
+      };
+    }
 
     res.json(user);
 
   } catch (error) {
+
     res.status(500).json({
       error: error.message
     });
+
   }
+
 });
-
-router.post("/:userId/equip", async (req, res) => {
-  try {
-    const { tools } = req.body;
-
-    const user = await User.findById(req.params.userId);
-
-    const selectedTools = await Tool.find({
-      name: { $in: tools }
-    });
-
-    const totalCost = selectedTools.reduce(
-      (acc, tool) => acc + tool.cost,
-      0
-    );
-
-    if (user.gold < totalCost) {
-      return res.status(400).json({
-        error: "Not enough gold"
-      });
-    }
-
-    user.gold -= totalCost;
-
-    user.equippedTools = tools;
-
-    await user.save();
-
-    res.json({
-      loadout: user.equippedTools,
-      remainingGold: user.gold
-    });
-
-  } catch (error) {
-    res.status(500).json({
-      error: error.message
-    });
-  }
-});
-
-module.exports = router;
